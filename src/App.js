a836ca32afad7d18ab34acba59df602d672dc234
fpmacedo <lipe_macedo@msn.com>
2019-12-16 16:13:36 -0300

    Finalizada aula de HookCallback

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 
 function App() {
+  // primeira posicao e o estado e a segunda e uma funcao para atualizar as infos desse estado
+  const [tech, setTech] = useState(['ReactJS', 'Reac-Native']);
+  const [newTech, setNewTech] = useState('');
+
+  const handleAdd = useCallback(() => {
+    setTech([...tech, newTech]);
+    setNewTech('');
+  }, [newTech, tech]);
+
+  useEffect(() => {
+    const storageTech = localStorage.getItem('tech');
+
+    if (storageTech) {
+      setTech(JSON.parse(storageTech));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('tech', JSON.stringify(tech));
+  }, [tech]);
+
+  const techsize = useMemo(() => tech.length, [tech]);
+
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <ul>
+        {tech.map(t => (
+          <li key={t}>{t}</li>
+        ))}
+      </ul>
+      <strong>Voce tem {techsize} tecnologias</strong>
+      <br />
+      <br />
+      <input value={newTech} onChange={e => setNewTech(e.target.value)} />
+      <button type="button" onClick={handleAdd}>
+        Adicionar
+      </button>
+    </>
   );
 }
 
